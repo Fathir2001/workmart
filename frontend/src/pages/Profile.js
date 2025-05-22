38ef2afee84487315dd85b6f864764b9ccafd100
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2025-05-22 22:56:55 +0530

    Refactor Profile component by removing Comments and Footer, and update ProfileView styles for improved layout and spacing.

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import ProfileView from '../components/ProfileView';
-import Comments from '../components/Comments';
-import Footer from '../components/Footer';
 
 const Profile =()=> {
   return (
     <div>
       <ProfileView/>
-      <Comments/>
-      <Footer/>
     </div>
   )
 }
